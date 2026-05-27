bc81aab95dcbd72b9fb6656efe15b2f99852e055
Hikaru <pg4c8gjzfk@privaterelay.appleid.com>
2026-05-27 13:31:59 +0900

    データを読み込むためのクラスを新設

---
+++ src/main/java/jp/ac/u_tokai/cc/javaadvanced/CsvDataLoder.java
@@ -0,0 +1,20 @@
+package jp.ac.u_tokai.cc.javaadvanced;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CsvDataLoder implements Dataloader {
+
+    @Override
+    public Map<String, Performance> load(File file){
+        //読み込んだデータを格納するための空のMapを用意
+        Map<String, Performance> Performance = new HashMap<>();
+        
+        
+        
+    }
+   
+
+    
+}
---
+++ src/main/java/jp/ac/u_tokai/cc/javaadvanced/Dataloader.java
@@ -0,0 +1,13 @@
+package jp.ac.u_tokai.cc.javaadvanced;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * データを読み込むための共通インターフェース
+ */
+
+public interface Dataloader {
+    // 処理を書かず、メソッドの名前と戻り値の「ルール」だけを定義する。
+    Map<String, Performance> load(File file);
+}
